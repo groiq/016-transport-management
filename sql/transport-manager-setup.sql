e8efad1190b9281c525cde82603073fefa785c26
groiq <35578027+groiq@users.noreply.github.com>
2019-12-10 15:24:39 +0100

    inserted sample load data

--- sql/transport-manager-setup.sql
+++ sql/transport-manager-setup.sql
@@ -23,14 +23,17 @@ CREATE TABLE loads (
     load_id INT PRIMARY KEY AUTO_INCREMENT,
     truck_id INT,
     start_location_id INT,
-    target_location_id int,
-    start_time_estimate TIMESTAMP default current_timestamp,
-    start_time_actual timestamp default current_timestamp,
-    target_time_estimate timestamp default current_timestamp,
-    target_time_actual timestamp default current_timestamp,
-    foreign key (truck_id) references trucks (truck_id),
-    foreign key (start_location_id) references locations (location_id),
-    foreign key (target_location_id) references locations (location_id)
+    target_location_id INT,
+    start_time_estimate TIMESTAMP null DEFAULT null,
+    start_time_actual TIMESTAMP null DEFAULT null,
+    -- target_time_estimate timestamp default current_timestamp, -- calculated value!
+    target_time_actual TIMESTAMP null DEFAULT null,
+    FOREIGN KEY (truck_id)
+        REFERENCES trucks (truck_id),
+    FOREIGN KEY (start_location_id)
+        REFERENCES locations (location_id),
+    FOREIGN KEY (target_location_id)
+        REFERENCES locations (location_id)
 );
 
 CREATE TABLE load_legs (
@@ -38,8 +41,8 @@ CREATE TABLE load_legs (
     load_id INT,
     location_id INT,
     number_in_sequence INT,
-    time_estimate timestamp default current_timestamp,
-    time_actual timestamp default current_timestamp,
+    time_estimate TIMESTAMP null DEFAULT null, -- calculated value!
+    time_actual TIMESTAMP null DEFAULT null,
     foreign key (load_id) references loads (load_id),
     foreign key (location_id) references locations (location_id)
 );
@@ -54,8 +57,12 @@ insert into locations (name,latitude,longitude) values ('Graz',47.066667,15.4333
 insert into locations (name,latitude,longitude) values ('Wien',48.208333,16.373056);
 insert into locations (name,latitude,longitude) values ('Eisenstadt',47.845556,16.518889);
 
-insert into trucks (fixed_costs_per_hour,cost_per_km,avg_speed) values (100.0,50.0,75.0);
+insert into trucks (fixed_cost_per_hour,cost_per_km,avg_speed) values (100.0,50.0,75.0);
 
+insert into loads (truck_id, start_location_id,target_location_id,start_time_estimate) values (1,3,8,current_timestamp);
 
+insert into load_legs (load_id,location_id,number_in_sequence,time_estimate) values (1,4,1,current_timestamp);
+insert into load_legs (load_id,location_id,number_in_sequence,time_estimate) values (1,6,2,current_timestamp);
 
--- select * from loads;
+select * from locations;
+select * from loads;
