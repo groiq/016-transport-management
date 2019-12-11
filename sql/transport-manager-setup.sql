64badd9ecea4ecbc4f9a66b8d112a8aaa7c0622c
groiq <35578027+groiq@users.noreply.github.com>
2019-12-11 15:28:58 +0100

    refined data setup and sample queries
    Data setup inserts a truck with a license plate.
    
    Sample queries use left join fror load legs.
    
    Set up sql file for local database user.

--- sql/transport-manager-setup.sql
+++ sql/transport-manager-setup.sql
@@ -58,7 +58,7 @@ insert into locations (name,latitude,longitude) values ('Graz',47.066667,15.4333
 insert into locations (name,latitude,longitude) values ('Wien',48.208333,16.373056);
 insert into locations (name,latitude,longitude) values ('Eisenstadt',47.845556,16.518889);
 
-insert into trucks (fixed_cost_per_hour,cost_per_km,avg_speed) values (100.0,50.0,75.0);
+insert into trucks (license_plate,fixed_cost_per_hour,cost_per_km,avg_speed) values ('LL-1234G',100.0,50.0,75.0);
 
 insert into loads (truck_id, start_location_id,target_location_id,start_time_estimate) values (1,3,8,current_timestamp);
 
@@ -85,7 +85,7 @@ FROM
     locations start_locations ON start_locations.location_id = loads.start_location_id
         JOIN
     locations target_locations ON target_locations.location_id = loads.target_location_id
-        JOIN
+        LEFT JOIN
     load_legs USING (load_id)
         JOIN
     locations leg_locations ON leg_locations.location_id = load_legs.location_id
---
+++ sql/user-commands.sql
@@ -0,0 +1,57 @@
+show tables;
+
+
+select * from locations;
+
+insert into locations (name,latitude,longitude) values ('equator',0,0);
+
+select * from locations;
+
+select * from locations where name = 'equator';
+delete from locations where location_id = 10;
+
+select * from locations;
+
+select * from loads;
+select * from trucks;
+
+SELECT 
+    load_id,
+    truck_id,
+    loads.start_location_id,
+    start_locations.name AS start_loc_name,
+    loads.target_location_id,
+    target_locations.name AS target_loc_name
+FROM
+    trucks
+        JOIN
+    loads USING (truck_id)
+        JOIN
+    locations start_locations ON start_locations.location_id = loads.start_location_id
+        JOIN
+    locations target_locations ON target_locations.location_id = loads.target_location_id
+;
+
+
+SELECT 
+    load_id,
+    truck_id,
+    loads.start_location_id,
+    start_locations.name AS start_loc_name,
+    loads.target_location_id,
+    target_locations.name AS target_loc_name,
+    leg_locations.location_id as leg_loc_id, leg_locations.name as leg_loc_name,
+    load_legs.number_in_sequence as leg_sequence_number
+FROM
+    trucks
+        RIGHT JOIN
+    loads USING (truck_id)
+        JOIN
+    locations start_locations ON start_locations.location_id = loads.start_location_id
+        JOIN
+    locations target_locations ON target_locations.location_id = loads.target_location_id
+        LEFT JOIN
+    load_legs USING (load_id)
+        LEFT JOIN
+    locations leg_locations ON leg_locations.location_id = load_legs.location_id
+;
