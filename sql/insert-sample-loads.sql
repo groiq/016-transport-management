037c27693ca46bd16f7682eb331b32109acf8805
groiq <35578027+groiq@users.noreply.github.com>
2019-12-13 17:18:18 +0100

    finished script to insert sample loads

--- sql/insert-sample-loads.sql
+++ sql/insert-sample-loads.sql
@@ -1,7 +1,20 @@
 
 /* Insert two sample loads into db, one with and one without legs */
 
-select * from loads;
-select * from load_legs;
+insert into loads (truck_id, start_location_id, target_location_id, start_time_estimate) values (1,1,2,'2019-12-12 08:00:00');  
+insert into load_legs 
+	(load_id, start_location_id, target_location_id, number_in_sequence, start_time_estimate) 
+    values (1,1,2,1,'2019-12-12 08:00:00');
 
--- insert into loads (truck_id, start_location_id, target_location_id) 
+insert into loads (truck_id, start_location_id, target_location_id, start_time_estimate) values (1,3,8,'2019-12-12 08:00:00');  
+insert into load_legs 
+	(load_id, start_location_id, target_location_id, number_in_sequence, start_time_estimate) 
+    values (2,3,4,1,'2019-12-12 08:00:00');
+insert into load_legs 
+	(load_id, start_location_id, target_location_id, number_in_sequence) 
+    values (2,4,6,2);
+insert into load_legs 
+	(load_id, start_location_id, target_location_id, number_in_sequence) 
+    values (2,6,8,3);
+
+
