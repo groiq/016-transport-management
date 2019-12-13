045f3324582bf13276690fddc6e05ea43fa8f249
groiq <35578027+groiq@users.noreply.github.com>
2019-12-13 16:54:26 +0100

    set up file for inserting sample loads

---
+++ sql/insert-sample-loads.sql
@@ -0,0 +1,7 @@
+
+/* Insert two sample loads into db, one with and one without legs */
+
+select * from loads;
+select * from load_legs;
+
+-- insert into loads (truck_id, start_location_id, target_location_id) 
