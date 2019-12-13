daed241bd835622516b514bed0c5eda87ed6c364
groiq <35578027+groiq@users.noreply.github.com>
2019-12-13 10:16:28 +0100

    reformat

--- index.php
+++ index.php
@@ -19,20 +19,20 @@
 
 <?php
 
-    // function for dropdown lists
-    function offerOptions($optionList, $optionValue, $optionName)
-    {
-        foreach ($optionList as $option) {
-            echo ("<option value='" . $option[$optionValue] . "'>" . $option[$optionName] . "</option>\n");
-        }
+// function for dropdown lists
+function offerOptions($optionList, $optionValue, $optionName)
+{
+    foreach ($optionList as $option) {
+        echo ("<option value='" . $option[$optionValue] . "'>" . $option[$optionName] . "</option>\n");
     }
+}
 
-    ?>
+?>
 
     <?php
 
     // connect to database 
-    
+
     // $con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "tms-database.mariadb.database.azure.com", "tmsadmin@tms-database", {your_password}, {your_database}, 3306);
     // $options = array(
     // PDO::MYSQL_ATTR_SSL_CA => '/var/www/html/BaltimoreCyberTrustRoot.crt.pem'
@@ -56,7 +56,7 @@
 
         // test output: first and last leg
         // echo($legs[0] . ' -> ' . $legs[$lastLegIndex] . "\n");
- 
+
         // insert a row into loads
         $statement = $pdo->prepare("INSERT INTO loads (start_location_id,target_location_id,truck_id,start_time_estimate) VALUES (?,?,?,?);");
         $timestamp = strtotime($_POST["date"] . " " . $_POST["time"]);
@@ -71,10 +71,9 @@
         // insert legs; start counting at 0
         for ($i = 0; $i < $lastLegIndex; $i++) {
             // echo("inserting: " . $legs[$i] . " -> " . $legs[$i+1] . " as leg #" . $i . "\n");
-            $statement = $pdo -> prepare("INSERT INTO load_legs (load_id, start_location_id, target_location_id, number_in_sequence) VALUES (?,?,?,?);");
-            $statement->execute(array($newLoadId,$legs[$i],$legs[$i+1],$i+1));
+            $statement = $pdo->prepare("INSERT INTO load_legs (load_id, start_location_id, target_location_id, number_in_sequence) VALUES (?,?,?,?);");
+            $statement->execute(array($newLoadId, $legs[$i], $legs[$i + 1], $i + 1));
         }
-
     }
 
     // read data
@@ -92,68 +91,70 @@
 
     <div class="container">
 
+        <!-- <div class="row"> -->
+            <h1>Transport Management System</h1>
 
+            <div class="card">
 
-    <h1>Transport Management System</h1>
+<h2>Neuer Transport</h2>
 
-    <h2>Neuer Transport</h2>
+            <form action="./index.php" method="post">
+                <input type="hidden" id="dbInsert" name="dbInsert" value="insertLoad">
 
-    <form action="./index.php" method="post">
-        <input type="hidden" id="dbInsert" name="dbInsert" value="insertLoad">
+                <div class="form-group">
 
-        <div class="form-group">
-
-            <!-- <label class="control-label" for="startTime">Abfahrt</label>
+                    <!-- <label class="control-label" for="startTime">Abfahrt</label>
             <input class="form-control" id="startTime" name="startTime" type="datetime-local"> -->
 
-            <label class="control-label" for="date">Datum</label>
-            <input class="form-control" id="date" name="date" placeholder="MM/DD/YYY" type="date" />
+                    <label class="control-label" for="date">Datum</label>
+                    <input class="form-control" id="date" name="date" placeholder="MM/DD/YYY" type="date" />
 
-            <label for="time">Uhrzeit</label>
-            <input class="form-control" type="time" id="time" name="time">
+                    <label for="time">Uhrzeit</label>
+                    <input class="form-control" type="time" id="time" name="time">
 
-            <label for="truck">Fahrzeug</label>
-            <select class="form-control" id="truck" name="truck">
-                <?php
-                offerOptions($trucks, "truck_id", "license_plate");
-                ?>
-            </select>
+                    <label for="truck">Fahrzeug</label>
+                    <select class="form-control" id="truck" name="truck">
+                        <?php
+                        offerOptions($trucks, "truck_id", "license_plate");
+                        ?>
+                    </select>
+
+                </div>
+                <div class="form-group">
+
+                    <div>
+                        <label for="startLocation">Start</label>
+                        <!-- <select class="form-control" id="startLocation" name="startLocation"> -->
+                        <select class="form-control" id="startLocation" name="legs[]">
+                            <!-- selectHTML = "<select class='form-control' id='legs[]' name='legs[]'>"; -->
+                            <?php
+                            offerOptions($locations, "location_id", "name");
+                            ?>
+                        </select>
+                    </div>
+                    <div>
+                        <div id="dynamicInput"></div>
+                        <input type="button" value="Zwischenhalt hinzuf&uuml;gen" onclick="addInput('dynamicInput');" />
+                        <div>
+                            <div>
+                                <label for="targetLocation">Ziel</label>
+                                <!-- <select class="form-control" id="targetLocation" name="targetLocation"> -->
+                                <select class="form-control" id="targetLocation" name="legs[]">
+                                    <?php
+                                    offerOptions($locations, "location_id", "name");
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+
+                        <!-- <input type="button" value="Save" /> -->
+
+                        <div class="form-group">
+                            <input type="submit" value="Transport erstellen" />
+                        </div>
+            </form>
 
         </div>
-        <div class="form-group">
-
-            <div>
-                <label for="startLocation">Start</label>
-                <!-- <select class="form-control" id="startLocation" name="startLocation"> -->
-                <select class="form-control" id="startLocation" name="legs[]">
-                <!-- selectHTML = "<select class='form-control' id='legs[]' name='legs[]'>"; -->
-                   <?php
-                    offerOptions($locations, "location_id", "name");
-                    ?>
-                </select>
-            </div>
-            <div>
-            <div id="dynamicInput"></div>
-        <input type="button" value="Zwischenhalt hinzuf&uuml;gen" onclick="addInput('dynamicInput');" />
-        <div>
-            <div>
-                <label for="targetLocation">Ziel</label>
-                <!-- <select class="form-control" id="targetLocation" name="targetLocation"> -->
-                <select class="form-control" id="targetLocation" name="legs[]">
-                    <?php
-                    offerOptions($locations, "location_id", "name");
-                    ?>
-                </select>
-            </div>
-        </div>
-
-        <!-- <input type="button" value="Save" /> -->
-
-        <div class="form-group">
-            <input type="submit" value="Transport erstellen" />
-        </div>
-    </form>
-
     </div>
 
     <!-- -------------------------------------------------- -->
