36c569eeec62f2288919e8d21871dc7a595b046f
groiq <35578027+groiq@users.noreply.github.com>
2019-12-13 11:04:27 +0100

    form is contained in card

--- index.php
+++ index.php
@@ -15,88 +15,91 @@
 
 <body>
 
-    <pre>
 
-<?php
+    <div id="controller">
 
-// function for dropdown lists
-function offerOptions($optionList, $optionValue, $optionName)
-{
-    foreach ($optionList as $option) {
-        echo ("<option value='" . $option[$optionValue] . "'>" . $option[$optionName] . "</option>\n");
-    }
-}
 
-?>
+        <?php
 
-    <?php
-
-    // connect to database 
-
-    // $con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "tms-database.mariadb.database.azure.com", "tmsadmin@tms-database", {your_password}, {your_database}, 3306);
-    // $options = array(
-    // PDO::MYSQL_ATTR_SSL_CA => '/var/www/html/BaltimoreCyberTrustRoot.crt.pem'
-    // );
-    // $pdo = new PDO('mysql:host=tms-database.mariadb.database.azure.com;port=3306;dbname=transport_management', 'tmsadmin@tms-database', 'nRfO4v7t6AOl5OORuXJm', $options);
-    /*
-	$options = array(
-	PDO::MYSQL_ATTR_SSL_CA => '/var/www/html/BaltimoreCyberTrustRoot.crt.pem'
-	);
-	$db = new PDO('mysql:host=mydemoserver.mysql.database.azure.com;port=3306;dbname=databasename', 'username@mydemoserver', 'yourpassword', $options);
-	*/
-    $pdo = new PDO('mysql:host=localhost:3306;dbname=transport_management', 'transport', 'transport_mgmt');
-    // $pdo = new PDO('mysql:host=tms-database.mariadb.database.azure.com:3306;dbname=transport_management', 'tmsadmin@tms-database', 'nRfO4v7t6AOl5OORuXJm');
-
-    // write to database if there's something in the form
-    if (!empty($_POST["dbInsert"])) {
-
-        // set convenience vars
-        $legs = $_POST['legs'];
-        $lastLegIndex = count($legs) - 1;
-
-        // test output: first and last leg
-        // echo($legs[0] . ' -> ' . $legs[$lastLegIndex] . "\n");
-
-        // insert a row into loads
-        $statement = $pdo->prepare("INSERT INTO loads (start_location_id,target_location_id,truck_id,start_time_estimate) VALUES (?,?,?,?);");
-        $timestamp = strtotime($_POST["date"] . " " . $_POST["time"]);
-        $sqlTimestamp = date('y-m-d H:i:s', $timestamp);
-        $statement->execute(array($legs[0], $legs[$lastLegIndex], $_POST['truck'], $sqlTimestamp));
-
-        // fetch id of new row
-        $query = $pdo->query("SELECT LAST_INSERT_ID();");
-        $queryResult = $query->fetchAll(\PDO::FETCH_ASSOC);
-        $newLoadId = $queryResult[0]['LAST_INSERT_ID()'];
-
-        // insert legs; start counting at 0
-        for ($i = 0; $i < $lastLegIndex; $i++) {
-            // echo("inserting: " . $legs[$i] . " -> " . $legs[$i+1] . " as leg #" . $i . "\n");
-            $statement = $pdo->prepare("INSERT INTO load_legs (load_id, start_location_id, target_location_id, number_in_sequence) VALUES (?,?,?,?);");
-            $statement->execute(array($newLoadId, $legs[$i], $legs[$i + 1], $i + 1));
+        // function for dropdown lists
+        function offerOptions($optionList, $optionValue, $optionName)
+        {
+            foreach ($optionList as $option) {
+                echo ("<option value='" . $option[$optionValue] . "'>" . $option[$optionName] . "</option>\n");
+            }
         }
-    }
 
-    // read data
-    $locationQuerySql = "select location_id, name from locations;";
-    $locationQuery = $pdo->query($locationQuerySql);
-    $locations = $locationQuery->fetchAll(\PDO::FETCH_ASSOC);
 
-    $truckQuerySql = "select truck_id, license_plate from trucks;";
-    $truckQuery = $pdo->query($truckQuerySql);
-    $trucks = $truckQuery->fetchAll(\PDO::FETCH_ASSOC);
+        // connect to database 
 
-    ?>
+        // $con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "tms-database.mariadb.database.azure.com", "tmsadmin@tms-database", {your_password}, {your_database}, 3306);
+        // $options = array(
+        // PDO::MYSQL_ATTR_SSL_CA => '/var/www/html/BaltimoreCyberTrustRoot.crt.pem'
+        // );
+        // $pdo = new PDO('mysql:host=tms-database.mariadb.database.azure.com;port=3306;dbname=transport_management', 'tmsadmin@tms-database', 'nRfO4v7t6AOl5OORuXJm', $options);
+        /*
+        $options = array(
+        PDO::MYSQL_ATTR_SSL_CA => '/var/www/html/BaltimoreCyberTrustRoot.crt.pem'
+        );
+        $db = new PDO('mysql:host=mydemoserver.mysql.database.azure.com;port=3306;dbname=databasename', 'username@mydemoserver', 'yourpassword', $options);
+        */
+        $pdo = new PDO('mysql:host=localhost:3306;dbname=transport_management', 'transport', 'transport_mgmt');
+        // $pdo = new PDO('mysql:host=tms-database.mariadb.database.azure.com:3306;dbname=transport_management', 'tmsadmin@tms-database', 'nRfO4v7t6AOl5OORuXJm');
 
-</pre>
+        // write to database if there's something in the form
+        if (!empty($_POST["dbInsert"])) {
+
+            // set convenience vars
+            $legs = $_POST['legs'];
+            $lastLegIndex = count($legs) - 1;
+
+            // test output: first and last leg
+            // echo($legs[0] . ' -> ' . $legs[$lastLegIndex] . "\n");
+
+            // insert a row into loads
+            $statement = $pdo->prepare("INSERT INTO loads (start_location_id,target_location_id,truck_id,start_time_estimate) VALUES (?,?,?,?);");
+            $timestamp = strtotime($_POST["date"] . " " . $_POST["time"]);
+            $sqlTimestamp = date('y-m-d H:i:s', $timestamp);
+            $statement->execute(array($legs[0], $legs[$lastLegIndex], $_POST['truck'], $sqlTimestamp));
+            $statement = null;
+
+            // fetch id of new row
+            $newLoadId = $pdo->lastInsertId();
+            // $query = $pdo->query("SELECT LAST_INSERT_ID();");
+            // $queryResult = $query->fetchAll(\PDO::FETCH_ASSOC);
+            // $newLoadId = $queryResult[0]['LAST_INSERT_ID()'];
+            // echo($newLoadId);
+
+            // insert legs; start counting at 0
+            for ($i = 0; $i < $lastLegIndex; $i++) {
+                // echo("inserting: " . $legs[$i] . " -> " . $legs[$i+1] . " as leg #" . $i . "\n");
+                $statement = $pdo->prepare("INSERT INTO load_legs (load_id, start_location_id, target_location_id, number_in_sequence) VALUES (?,?,?,?);");
+                $statement->execute(array($newLoadId, $legs[$i], $legs[$i + 1], $i + 1));
+                $statement = null;
+            }
+        }
+
+
+        // read data
+        $locationQuerySql = "select location_id, name from locations;";
+        $locationQuery = $pdo->query($locationQuerySql);
+        $locations = $locationQuery->fetchAll(\PDO::FETCH_ASSOC);
+
+        $truckQuerySql = "select truck_id, license_plate from trucks;";
+        $truckQuery = $pdo->query($truckQuerySql);
+        $trucks = $truckQuery->fetchAll(\PDO::FETCH_ASSOC);
+
+        ?>
+
+    </div>
 
     <div class="container">
 
-        <!-- <div class="row"> -->
-            <h1>Transport Management System</h1>
+        <h1>Transport Management System</h1>
 
-            <div class="card">
+        <div class="card">
 
-<h2>Neuer Transport</h2>
+            <h2>Neuer Transport</h2>
 
             <form action="./index.php" method="post">
                 <input type="hidden" id="dbInsert" name="dbInsert" value="insertLoad">
@@ -152,15 +155,21 @@ function offerOptions($optionList, $optionValue, $optionName)
                         <div class="form-group">
                             <input type="submit" value="Transport erstellen" />
                         </div>
+                    </div>
             </form>
+            <!-- </div> -->
 
         </div>
+
     </div>
 
-    <!-- -------------------------------------------------- -->
-    <h1>Debug data</h1>
 
-    <pre>
+
+    <div id="debug">
+
+        <h1>Debug data</h1>
+
+        <pre>
         <?php
 
         if (!empty($_POST["dbInsert"])) {
@@ -183,13 +192,15 @@ function offerOptions($optionList, $optionValue, $optionName)
 
         echo ("\n\n");
 
-        print_r($locations);
+        // print_r($locations);
 
-        var_dump($locations);
+        // var_dump($locations);
 
         ?>
-    </pre>
-    <!-- -------------------------------------------------- -->
+        </pre>
+
+
+    </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
