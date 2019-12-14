5e225662a5d7b9906b8412772c133d02e39d2469
groiq <35578027+groiq@users.noreply.github.com>
2019-12-15 00:45:18 +0100

    styled the page

--- index.php
+++ index.php
@@ -17,6 +17,8 @@
 
 <body>
 
+<!-- <div class="bg">
+</div> -->
 
     <div id="controller">
 
@@ -101,15 +103,16 @@
             TMS
         </h1>
     </header> -->
-    <div class="jumbotron jumbotron-fluid bg-primary text-white">
-        <div class="container">
-            <h1 class="display-4">
+    <div class="fixed-top container container-fluid bg-primary text-white">
+        <!-- <div class="container"> -->
+            <!-- <div class="maxwidth"> -->
+            <h1>
                 TMS
             </h1>
-        </div>
+        <!-- </div> -->
     </div>
 
-    <div class="container d-flex flex-column">
+    <div class="container d-flex flex-column" id="main">
         <!-- <div class="container"> -->
 
        <!-- <div class="row"> -->
@@ -118,8 +121,8 @@
 
         <!-- <div class="row"> -->
 
-        <!-- <div class="card col-lg"> -->
-        <div class="align-self-center" style="max-width: 500px">
+        <!-- <div class="card col-lg"> --> 
+        <div class="align-self-center maxwidth p-1" id="form">
             <!-- <div> -->
 
             <h2 class="">Neuer Transport</h2>
@@ -242,11 +245,13 @@
 
     </div>
 
+    <!-- </div> -->
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script> -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script> -->
+    <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script> -->
 
     <!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
@@ -279,7 +284,7 @@
             var labelAttr = document.createAttribute('for');
             labelAttr.nodeValue = 'leg';
             labelDiv.setAttributeNode(labelAttr);
-            labelDiv.innerHTML = 'Etappe:';
+            labelDiv.innerHTML = elemCounter + '. Etappe:';
             newDiv.appendChild(labelDiv);
 
             var selectDiv = document.createElement('select');
