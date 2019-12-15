f16e5f069e67719b60304c1b9ecbcc49c23564ab
groiq <35578027+groiq@users.noreply.github.com>
2019-12-15 11:31:01 +0100

    forgot test output for the last commit

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 
-    <title>TMS-04</title>
+    <title>TMS-05</title>
 </head>
 
 <body>
