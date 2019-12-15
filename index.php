a3fad70454ee6ae685e86f67fc738883693f519b
groiq <35578027+groiq@users.noreply.github.com>
2019-12-15 11:52:37 +0100

    trying status bar style one more time

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
     <!-- <link rel="apple-touch-icon" href="./apple-touch-icon.png"> -->
     <link rel="apple-touch-startup-image" href="./imgs/launch.png">
     <meta name="apple-mobile-web-app-capable" content="yes">
-    <!-- <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"> -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 
-    <title>TMS</title>
+    <title>TMS one more</title>
 </head>
 
 <body>
