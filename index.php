8e6005fc34ee10a39e7ef9f928cad48c4b1c5b77
groiq <35578027+groiq@users.noreply.github.com>
2019-12-15 11:15:25 +0100

    set background color to match header background

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 
-    <title>TMS-01</title>
+    <title>TMS-02</title>
 </head>
 
 <body>
