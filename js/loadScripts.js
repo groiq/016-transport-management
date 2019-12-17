f0d044b237dbb8a298d26f0b0d680a63985ea0eb
groiq <35578027+groiq@users.noreply.github.com>
2019-12-17 09:15:05 +0100

    writeDuration is parametrized

--- js/loadScripts.js
+++ js/loadScripts.js
@@ -8,8 +8,8 @@ $(document).ready(function(){
     setInterval(updateTime, 1000);
     
     var duration = 0;
-    function writeDuration() {
-        var durationLeft = duration;
+    function writeDuration(durationParam) {
+        var durationLeft = durationParam;
         var hours = Math.floor(durationLeft / 3600);
         durationLeft -= (hours * 3600);
         var minutes = Math.floor(durationLeft / 60);
@@ -18,18 +18,13 @@ $(document).ready(function(){
         var result = hours.toString().padStart(2,'0') + ":" 
             + minutes.toString().padStart(2,'0') + ":" 
             + seconds.toString().padStart(2,'0');
-        // alert(result);
         return result;
-        // var hours = Math.floor((duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        // var minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
-        // var seconds = Math.floor((duration % (1000 * 60)) / 1000);
-        // $('.trackDuration').html(hours.toString().padStart(2,'0') + ":" + minutes.toString().padStart(2,'0') + ":" + seconds.toString().padStart(2,'0'));
     }
     
     setInterval(function() {
         duration += 1;
         // writeDuration();
-        $('.trackDuration').html(writeDuration());
+        $('.trackDuration').html(writeDuration(duration));
     }, 1000);
 
     $('div.currentLeg .future').hide();
@@ -40,14 +35,11 @@ $(document).ready(function(){
     $('button').click(function(){
 
         // get elements by class and pick the first one
+        // (there should be only one .currentLeg element)
         var oldLegId = document.getElementsByClassName("currentLeg")[0].id;
-        // var oldLegNum = Number(oldLeg);
-        // alert(oldLegNum);
         var newLegId = (Number(oldLegId)+1).toString();
-        // alert(nextLeg);
         var oldLegSelector = '#' + oldLegId;
         var newLegSelector = '#' + newLegId;
-        // alert(oldLegSelector);
 
         $(oldLegSelector).removeClass('currentLeg');
         $(oldLegSelector).addClass('pastLeg');
@@ -57,10 +49,10 @@ $(document).ready(function(){
         $(oldLegSelector + ' .current').hide();
         $(newLegSelector + ' .future').hide();
   
-        $(oldLegSelector + ' .past-duration').html(writeDuration());
+        $(oldLegSelector + ' .past-duration').html(writeDuration(duration));
         duration = 0;
         // alert(writeDuration);
-        $('.trackDuration').html(writeDuration());
+        $('.trackDuration').html(writeDuration(duration));
         var now = new Date();
         $(oldLegSelector + ' .past-timestamp').html(now.toLocaleTimeString());
 
