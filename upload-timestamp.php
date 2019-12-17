f310273f05c0b15ad8881d9b0acd173ddbc98131
groiq <35578027+groiq@users.noreply.github.com>
2019-12-17 17:55:16 +0100

    reroute db

--- upload-timestamp.php
+++ upload-timestamp.php
@@ -3,8 +3,8 @@
 <?php
 
 // connect to database 
-$pdo = new PDO('mysql:host=localhost:3306;dbname=transport_management', 'transport', 'transport_mgmt');
-// $pdo = new PDO('mysql:host=tms-database.mariadb.database.azure.com:3306;dbname=transport_management', 'tmsadmin@tms-database', 'nRfO4v7t6AOl5OORuXJm');
+// $pdo = new PDO('mysql:host=localhost:3306;dbname=transport_management', 'transport', 'transport_mgmt');
+$pdo = new PDO('mysql:host=tms-database.mariadb.database.azure.com:3306;dbname=transport_management', 'tmsadmin@tms-database', 'nRfO4v7t6AOl5OORuXJm');
 
 $sqlTimestamp = date('y-m-d H:i:s', time());
 $statement = $pdo->prepare("call add_timestamp(?,?,?);");
